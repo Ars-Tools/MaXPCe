8dc31e49841af7337daf8244b17cd428c0411cb6
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-11-25 23:49:50 +0900

    xpc compatible

---
+++ MXS/Sources/XPC.swift
@@ -0,0 +1,27 @@
+//
+//  XPC.swift
+//  MaXPCe
+//
+//  Created by Kota on 11/24/25.
+//
+import XPC
+public protocol XPCObject: Sendable {
+    init(xpc object: xpc_object_t)
+    var xpc: xpc_object_t { get }
+}
+extension String {
+    @inlinable
+    public init(xpc object: xpc_object_t) {
+        self.init(decoding: UnsafeBufferPointer(start: xpc_string_get_string_ptr(object), count: xpc_string_get_length(object)).withMemoryRebound(to: UInt8.self, \.self), as: UTF8.self)
+    }
+}
+extension xpc_connection_t {
+    @inlinable
+    func notify(list: Array<Atom>) {
+        precondition(xpc_get_type(self) == XPC_TYPE_CONNECTION)
+        let req = xpc_dictionary_create_empty()
+        xpc_dictionary_set_string(req, "/", "n")
+        xpc_dictionary_set_value(req, "=", list.xpc)
+        xpc_connection_send_message(self, req)
+    }
+}
