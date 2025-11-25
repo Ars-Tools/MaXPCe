b1777a519f1e4786900a28598f0c2835474b52dd
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-11-25 23:50:20 +0900

    shared memory manager for msp

---
+++ MXS/Sources/Buffer.swift
@@ -0,0 +1,44 @@
+//
+//  Buffer.swift
+//  MaXPCe
+//
+//  Created by Kota on 11/24/25.
+//
+import XPC
+public final class Buffer: @unchecked Sendable, XPCObject {
+    public let start: UnsafeMutableRawPointer
+    public let count: Int
+    @inlinable
+    public init(xpc object: xpc_object_t) {
+        precondition(xpc_get_type(object) == XPC_TYPE_SHMEM)
+        var`catch`: UnsafeMutableRawPointer?
+        count = xpc_shmem_map(object, &`catch`)
+        start = unsafeBitCast(`catch`, to: UnsafeMutableRawPointer.self)
+    }
+    public init() {
+        start = unsafeBitCast(Optional<UnsafeMutableRawPointer>.none, to: UnsafeMutableRawPointer.self)
+        count = 0
+    }
+    deinit {
+        munmap(start, count)
+    }
+}
+extension Buffer {
+    public var xpc: xpc_object_t {
+        xpc_shmem_create(start, count)
+    }
+}
+extension Buffer {
+    @inlinable
+    public var unsafeMutableRawBufferPointer: UnsafeMutableRawBufferPointer {
+        .init(start: start, count: count)
+    }
+    @inlinable
+    public func withUnsafeMutableRawBufferPointer<E, R>(_ body: (UnsafeMutableRawBufferPointer) throws (E) -> R) rethrows -> R {
+        try body(unsafeMutableRawBufferPointer)
+    }
+    @inlinable
+    public func withUnsafeMutableBufferPointer<T: BitwiseCopyable, E, R>(_ body: (UnsafeMutableBufferPointer<T>) throws (E) -> R) rethrows -> R {
+        try body(unsafeMutableRawBufferPointer.assumingMemoryBound(to: T.self))
+    }
+}
