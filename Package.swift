78303b3d52a40fbbe125e830a3c4449660a45688
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2026-08-06 16:50:10 +0900

    refactor

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,24 +6,54 @@ import PackageDescription
 let package = Package(
     name: "MaXPCe",
     platforms: [
-        .macOS(.v15)
+        .macOS(.v26)
     ],
     products: [
-        .library(name: "MaXPCe.MXO", targets: ["MXO"])
+        .library(name: "MaXPCe.MXO", targets: ["MXO"]),
+        .library(name: "MaXPCe.MXI", targets: ["MXI"]),
     ],
     targets: [
+        .target(
+            name: "MXO",
+            dependencies: [
+                "MAX"
+            ],
+            path: "MXO/Sources"
+        ),
         .executableTarget(
             name: "MXO-Example",
             dependencies: ["MXO"],
-            path: "MXO/Example"
-        ),
-        .target(
-            name: "MXO",
-            path: "MXO/Sources"
+            path: "MXO/Example",
+            cSettings: [
+                .define("ACCELERATE_NEW_LAPACK"),
+                .define("ACCELERATE_LAPACK_ILP64")
+            ]
         ),
         .target(
             name: "MXI",
-            path: "MXI/Sources"
+            dependencies: ["MXO"],
+            path: "MXI/Sources",
+            resources: [
+                .process("../Resources")
+            ]
+        ),
+        .testTarget(
+            name: "MXITests",
+            dependencies: [
+                "MXI"
+            ],
+            path: "MXI/Tests"
+        ),
+        .target(
+            name: "MAX",
+            path: "MAX/Sources"
+        ),
+        .testTarget(
+            name: "MAX-Tests",
+            dependencies: [
+                "MAX"
+            ],
+            path: "MAX/Tests"
         )
     ]
 )
